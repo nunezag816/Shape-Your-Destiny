24d73e41fcceba74365c97b978250fe63c00ee4a
nunezag816 <nunezag@msoe.edu>
2025-06-22 20:47:36 -0500

    Update Rectangle.java

--- app/src/main/java/org/example/Rectangle.java
+++ app/src/main/java/org/example/Rectangle.java
@@ -1,15 +1,25 @@
 package org.example;
 
-public class Rectangle extends Shape {
+public class Rectangle extends Shape implements Polygon {
     protected double length, width;
+
     public Rectangle(double length, double width) {
         this.length = length;
         this.width = width;
     }
+
+    @Override
     public double getArea() {
         return length * width;
     }
+
+    @Override
     public double getPerimeter() {
         return 2 * (length + width);
+    }
+
+    @Override
+    public int numberOfSides() {
+        return 4;
     }
 }
