d123086a54864e0ff1b9b49a713e500a798debb8
nunezag816 <nunezag@msoe.edu>
2025-06-22 20:49:36 -0500

    Create Parallelogram.java

---
+++ app/src/main/java/org/example/Parallelogram.java
@@ -0,0 +1,24 @@
+public class Parallelogram extends Shape implements Polygon {
+    private double base, side, height;
+
+    public Parallelogram(double base, double side, double height) {
+        this.base = base;
+        this.side = side;
+        this.height = height;
+    }
+
+    @Override
+    public double getArea() {
+        return base * height;
+    }
+
+    @Override
+    public double getPerimeter() {
+        return 2 * (base + side);
+    }
+
+    @Override
+    public int numberOfSides() {
+        return 4;
+    }
+}
