94d5ca5fc060512ca61622c4b5d0c71307899b6f
nunezag816 <nunezag@msoe.edu>
2025-06-22 20:38:58 -0500

    Create Rectangle.java

---
+++ app/src/main/java/org/example/Rectangle.java
@@ -0,0 +1,13 @@
+public class Rectangle extends Shape {
+    protected double length, width;
+    public Rectangle(double length, double width) {
+        this.length = length;
+        this.width = width;
+    }
+    public double getArea() {
+        return length * width;
+    }
+    public double getPerimeter() {
+        return 2 * (length + width);
+    }
+}
