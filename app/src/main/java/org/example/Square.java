a3cafdc57eaa2633918e5b91cf8b53e03e68dd95
nunezag816 <nunezag@msoe.edu>
2025-06-22 20:47:58 -0500

    Update Square.java

--- app/src/main/java/org/example/Square.java
+++ app/src/main/java/org/example/Square.java
@@ -1,5 +1,11 @@
-public class Square extends Rectangle {
+public class Square extends Rectangle implements Polygon {
+
     public Square(double side) {
         super(side, side);
+    }
+
+    @Override
+    public int numberOfSides() {
+        return 4;
     }
 }
