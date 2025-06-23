c91db487b2df35c195d0a61c02da35848cf76fb8
nunezag816 <nunezag@msoe.edu>
2025-06-22 20:53:36 -0500

    Update PolygonTest.java

--- app/src/test/java/org/example/PolygonTest.java
+++ app/src/test/java/org/example/PolygonTest.java
@@ -7,31 +7,38 @@ public class PolygonTest {
 
     @Test
     public void testRectangleSides() {
-        Polygon poly = new Rectangle(1, 2);
-        assertEquals(4, poly.numberOfSides());
+        Polygon p = new Rectangle(2, 3);
+        assertEquals(4, p.numberOfSides());
     }
 
     @Test
     public void testSquareSides() {
-        Polygon poly = new Square(3);
-        assertEquals(4, poly.numberOfSides());
+        Polygon p = new Square(4);
+        assertEquals(4, p.numberOfSides());
     }
 
     @Test
     public void testRightTriangleSides() {
-        Polygon poly = new RightTriangle(3, 4);
-        assertEquals(3, poly.numberOfSides());
+        Polygon p = new RightTriangle(3, 4);
+        assertEquals(3, p.numberOfSides());
     }
 
     @Test
     public void testIsoscelesRightTriangleSides() {
-        Polygon poly = new IsoscelesRightTriangle(4);
-        assertEquals(3, poly.numberOfSides());
+        Polygon p = new IsoscelesRightTriangle(5);
+        assertEquals(3, p.numberOfSides());
     }
 
     @Test
     public void testParallelogramSides() {
-        Polygon poly = new Parallelogram(5, 3, 2);
-        assertEquals(4, poly.numberOfSides());
+        Polygon p = new Parallelogram(5, 3, 2);
+        assertEquals(4, p.numberOfSides());
+    }
+
+    // Negative Test: Circle does NOT implement Polygon
+    @Test
+    public void testCircleDoesNotImplementPolygon() {
+        Circle c = new Circle(1);
+        assertFalse(c instanceof Polygon);
     }
 }
