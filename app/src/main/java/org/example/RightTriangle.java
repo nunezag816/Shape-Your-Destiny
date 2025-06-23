7943f32cbccadda992630a9d15f50fbe4b7006ba
nunezag816 <nunezag@msoe.edu>
2025-06-22 20:39:32 -0500

    Create RightTriangle.java

---
+++ app/src/main/java/org/example/RightTriangle.java
@@ -0,0 +1,13 @@
+public class RightTriangle extends Shape {
+    protected double leg1, leg2;
+    public RightTriangle(double leg1, double leg2) {
+        this.leg1 = leg1;
+        this.leg2 = leg2;
+    }
+    public double getArea() {
+        return 0.5 * leg1 * leg2;
+    }
+    public double getPerimeter() {
+        return leg1 + leg2 + Math.sqrt(leg1*leg1 + leg2*leg2);
+    }
+}
